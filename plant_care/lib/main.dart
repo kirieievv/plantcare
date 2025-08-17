a8cb25b8bf41a1186c8d3578a548d0ccd44727b8
Vladymyr <krv@mbpvladymyr8693.speedport.ip>
2025-08-17 18:20:56 +0200

    Veleicht gut

--- plant_care/lib/main.dart
+++ plant_care/lib/main.dart
@@ -14,7 +14,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
   // Load environment variables
-  await dotenv.load(fileName: "assets/.env");
+  await dotenv.load(fileName: ".env");
   
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
